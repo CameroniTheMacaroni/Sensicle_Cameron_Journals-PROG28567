89c212263d053ebb25981a1eeaa7134b43062a90
LAPTOP-2V0SBDQ4\Cameron <camsenfort@gmail.com>
2026-09-10 17:10:38 -0400

    made a square appear on click

--- Assets/Scripts/SquareSpawner.cs
+++ Assets/Scripts/SquareSpawner.cs
@@ -3,6 +3,10 @@ using UnityEngine.InputSystem;
 
 public class SquareSpawner : MonoBehaviour
 {
+    
+    public bool clicked = false;
+    public Vector2 squareSizeX = new Vector2(0.2f, 0);
+    public Vector2 squareSizeY = new Vector2(0, 0.2f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,8 +21,22 @@ public class SquareSpawner : MonoBehaviour
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Debug.Log(mousePos);
-            
+            clicked = true;
+
+            drawSquare(mousePos);
         }
+        else
+        {
+            clicked = false;
+        }
+    }
+
+    public void drawSquare(Vector2 mousepos)
+    {
+        Debug.DrawLine(mousepos + squareSizeX, mousepos + squareSizeY, Color.red);//top Left
+        Debug.DrawLine(mousepos - squareSizeX, mousepos + squareSizeY, Color.red);//top Right
+        Debug.DrawLine(mousepos + squareSizeX, mousepos - squareSizeY, Color.red);//bottom Left
+        Debug.DrawLine(mousepos - squareSizeX, mousepos - squareSizeY, Color.red);//bottom Right
     }
     
 }
