65a02c1ea3e16e5048ecbf768eba23de622d5f7f
LAPTOP-2V0SBDQ4\Cameron <camsenfort@gmail.com>
2026-09-11 14:01:23 -0400

    all works :D

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class SquareScript : MonoBehaviour
 {
-    public Vector3 squareSizeX = new Vector3(0.2f, 0, 0);
-    public Vector3 squareSizeY = new Vector3(0, 0.2f, 0);
+    public Vector3 squareSizeX;
+    public Vector3 squareSizeY;
+
+    public GameObject squarespawner;
     void Start()
     {
-        
+        squarespawner = GameObject.Find("SquareSpawner");
+
+        squareSizeX = squarespawner.GetComponent<SquareSpawner>().squareSizeX;
+        squareSizeY = squarespawner.GetComponent<SquareSpawner>().squareSizeY;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Debug.DrawLine(transform.position + squareSizeX, transform.position + squareSizeY, Color.red);//top Left
         Debug.DrawLine(transform.position - squareSizeX, transform.position + squareSizeY, Color.red);//top Right
         Debug.DrawLine(transform.position + squareSizeX, transform.position - squareSizeY, Color.red);//bottom Left
