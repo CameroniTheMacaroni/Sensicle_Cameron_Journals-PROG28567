d9b43270f077bdd176509533204cd5b120c63af6
LAPTOP-2V0SBDQ4\Cameron <camsenfort@gmail.com>
2026-09-25 16:50:02 -0400

    Task 4 complete

--- Assets/Scripts/Jounal3/Stars.cs
+++ Assets/Scripts/Jounal3/Stars.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
@@ -11,14 +12,49 @@ public class Stars : MonoBehaviour
     private Vector3 startPosition;
     private Vector3 endPosition;
 
+    public int currentStar;
+    public float linePercentage;
+    public float lineDrawSpeed = 0.01f;
+
+    public bool constellationDrawn;
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (!constellationDrawn)
+        {
+
+            //StartCoroutine(Drawconstellation());
+            DrawConstellation();
+            //constellationDrawn = true;
+        }
     }
 
-    private void DrawConstellation()
+    public void DrawConstellation()
     {
+        startPosition = starTransforms[currentStar].position;
+        endPosition = starTransforms[currentStar + 1].position;
 
+        Debug.DrawLine(startPosition, Vector3.Lerp(startPosition, endPosition, linePercentage), Color.purple);
+        for (int i = 0; i < currentStar; ++i)
+        {
+            Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position, Color.purple);
+        }
+        //Debug.Log(Vector3.Lerp(startPosition, endPosition, 0));
+        linePercentage += lineDrawSpeed;
+
+        if(linePercentage >= 1)
+        {
+            if(currentStar == 5)
+            {
+                currentStar = 0;
+            }
+            else
+            {
+                currentStar++;
+            }
+            linePercentage = 0;
+
+        }
     }
 }
